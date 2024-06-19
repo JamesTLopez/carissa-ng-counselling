625fb56d3be5aa96ac98358f25e81fa6e162632d
JamesTLopez <james.theo.lopez@gmail.com>
2024-06-18 23:23:13 -0400

    session card

---
+++ src/assets.js
@@ -0,0 +1,78 @@
+export const specialties = [
+  "Cultural Identity",
+  "Anger Management",
+  "Anxiety",
+  "Depression",
+  "Life Transitions",
+  "Personal Identity/GrowthSelf-Harm",
+];
+
+export const languages = ["english"];
+
+export const clientFocus = ["Adult Therapy", "Individuals", "Teen Therapy"];
+
+export const sessionFormats = [
+  {
+    title: "In-Person Therapy",
+    img: "https://images.unsplash.com/photo-1604881988758-f76ad2f7aac1?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "$120 + HST",
+    description:
+      "I want you to feel acknowledged, understood, and supported. </br>You deserve a safe space to navigate and process life's challenges.",
+  },
+  {
+    title: "Online Therapy",
+    img: "https://images.unsplash.com/photo-1588196749597-9ff075ee6b5b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: "$120 + HST",
+    description:
+      "I want you to feel acknowledged, understood, and supported. </br>You deserve a safe space to navigate and process life's challenges.",
+  },
+];
+
+export const therapies = [
+  "Acceptance & Commitment Therapy",
+  "Anti-Oppressive",
+  "Attachment Based Therapy",
+  "Dialectic Behaviour Therapy",
+  "Emotionally Focused Therapy",
+  "Humanistic",
+  "Integrative or Holistic Therapy",
+  "Internal Family Systems",
+  "Interpersonal Therapy",
+  "Mindfulness Approach",
+  "Narrative Therapy",
+  "Psychodynamic",
+  "Strength Based",
+  "Trauma Informed Therapy",
+];
+
+export const supportAreas = [
+  "Anxiety",
+  "Depression",
+  "Grief/Loss",
+  "Panic Attacks",
+  "Stress/Burnout",
+  "Trauma",
+  "Mood Disorders",
+  "Social Anxiety",
+  "Body Image Concerns",
+  "Break Ups",
+  "BIPOC Experiences",
+  "Boundaries",
+  "Chronic Illness",
+  "Co-Dependency",
+  "Communication",
+  "Cross Cultural Challenges",
+  "Death",
+  "Divorce/Separation",
+  "Family Conflict",
+  "Men's Issues",
+  "Woman's Issues",
+  "Suicide Ideation",
+  "Life Transitions",
+  "Loneliness",
+  "Perfectionism",
+  "Personal Identity/Growth",
+  "Racial Identity",
+  "School Challenges",
+  "Self-Esteem/Self-Worth",
+];
