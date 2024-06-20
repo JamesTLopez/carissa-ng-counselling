c8a0330aa5dbd9a563816c5c740d110e4ac10f4c
JamesTLopez <james.theo.lopez@gmail.com>
2024-06-20 14:15:42 -0400

    svg animations

--- src/scripts/landing.js
+++ src/scripts/landing.js
@@ -1,11 +1,49 @@
 import anime from "animejs/lib/anime.es.js";
 
 anime({
-  targets: "#heart .path",
+  targets: "#l1 .path",
   strokeDashoffset: [anime.setDashoffset, 0],
-  easing: "easeInOutSine",
-  duration: 2000,
-  delay: (el, i) => 400,
-  direction: "normal",
+  easing: "linear",
+  duration: 700,
+  delay: (el, i) => {
+    return i * 60;
+  },
+  direction: "alternate",
+  loop: false,
+});
+
+anime({
+  targets: "#l2 .path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "linear",
+  duration: 700,
+  delay: (el, i) => {
+    return i * 60;
+  },
+  direction: "alternate",
+  loop: false,
+});
+
+anime({
+  targets: "#l3 .path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "linear",
+  duration: 700,
+  delay: (el, i) => {
+    return i * 60;
+  },
+  direction: "alternate",
+  loop: false,
+});
+
+anime({
+  targets: "#l4 .path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "linear",
+  duration: 700,
+  delay: (el, i) => {
+    return i * 60;
+  },
+  direction: "alternate",
   loop: false,
 });
