5d0b8c903686ea1f360c9041de1d9bb4edba9560
JamesTLopez <james.theo.lopez@gmail.com>
2024-07-08 18:44:03 -0400

    feat: header scroll animations

---
+++ src/scripts/observer.js
@@ -0,0 +1,35 @@
+const initObserver = () => {
+  const setCurrent = (section) => {
+    // remove all current tag's navitems
+    document.querySelectorAll(".current").forEach((el) => {
+      el.classList.remove("current");
+    });
+    // Add class to navitem in viewport
+    document
+      .querySelector(`.link[href="#${section.id}"]`)
+      .classList.add("current");
+  };
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      const currentScrollPosition = window.pageYOffset;
+      console.log("yes----", currentScrollPosition);
+
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          setCurrent(entry.target);
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    },
+    { threshold: 0.3 }
+  );
+
+  const elements = document.querySelectorAll("section");
+
+  elements.forEach((el) => observer.observe(el));
+};
+
+export default initObserver;
